44be74eecf214fcd565eea31876ddd5f1bde646c
Henry <3794634+Hlanier02@users.noreply.github.com>
2019-02-26 20:03:18 -0500

    This modification changes the basic code in order to get input from the user and display it back. This is a basic proof of concept.
    Next step is to manipulate it when in dd\dd\dd - dd\dd\dd n format.

--- Core.java
+++ Core.java
@@ -9,19 +9,92 @@ import java.util.*;
 public class Core 
 {	// beginning of class
 	
-	ArrayList output = new ArrayList<String>();
-	 FileReader something;//This will create the variable used by  method
-	//Since the FileReader method can only accept certain characters 
-	//Scanner is needed to handle the primatives in an efficient manner
-	 Scanner input; 
-	 
-	 try {
-		   something = new FileReader("something.txt");  // create the stream
-		   input = new Scanner("something.txt");
-		}
-	catch (FileNotFoundException e) 
-	{
-	   System.out.println("File not Found"); // do something to handle the error -- maybe, end the program
-	}
+	
+	/*		ArrayList output = new ArrayList<String>();
+		
+			 FileReader something;//This will create the variable used by  method
+			//Since the FileReader method can only accept certain characters 
+			//Scanner is needed to handle the primatives in an efficient manner
+		
+			 Scanner input; 
+			 
+			 try {
+				   something = new FileReader("something.txt");  // create the stream
+				   input = new Scanner( new File ("something.txt"));
+				}s
+			catch (FileNotFoundException e) 
+			{
+			   System.out.println("File not Found"); // do something to handle the error -- maybe, end the program
+			}
 			
-}//end of clas
+			*/
+		
+// System Input Utility -- Scanner
+	 static Scanner Input = new Scanner(System.in);
+//	 	static in = Input.nextLine();
+	
+		private static String InputText;
+ //Getter and Setter for InputText	
+		
+		private static String GetInputText () {
+			
+			return InputText;
+			
+		}
+		
+		private static void SetInputText(String text) {
+					
+					InputText=text; 
+		}
+		
+		
+			// Test Case  1
+					/*
+					 * Output user input after gathering it from user input
+					 * 
+					 *
+					 */
+			//Instructions for use
+					private static String Stuff = "Thank you for using this program...Please enter information..";
+					
+					static void Instructions(String x) {
+						x = Stuff;
+						System.out.printf(x);//This prints out instructions given (a String) from the x variable
+						System.out.println();
+						SetInputText(Input.nextLine());
+						Instructions(1);
+						Instructions(2);
+						
+						
+					}
+					static void Instructions(int n) {
+						if (n == 1) { //option 1 prints out what was entered
+							System.out.println("This is what you wrote: " + GetInputText()); 
+						}
+						else{//option 2 ends the program with a fond farewell
+							System.out.println("Thanks again for using this program  :) !");
+						}
+						
+					}
+					
+
+					
+		public static void main (String args[]) {
+			
+			/* With Static Methods!!
+			 * Core Start = new Core(); 
+				Start.Instructions(Stuff);
+				GetInputText(Input.nextLine());
+				Start.Instructions(1);
+				Start.Instructions(2);
+			 * */
+			
+			
+			//Without Static Methods
+			Instructions(Stuff);
+
+		
+	
+
+	}		
+}//end of class
